a0c5092d169217e88417f3428062cbf0154104a0
NSPOLE01 <89700736+NSPOLE01@users.noreply.github.com>
2025-06-17 18:25:43 -0400

    initial

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Nikhil Polepalli on 6/17/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
